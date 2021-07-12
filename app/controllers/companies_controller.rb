f74b855cd79ce80128e12fc2fc6b4f9096d54c3e
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2021-07-12 18:38:58 -0300

    Beginning build the form from create company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,16 +1,31 @@
 class CompaniesController < ApplicationController
   def new
+    @company = current_user.build_company
   end
 
   def edit
   end
 
   def create
-    
+    @company = current_user.build_company(company_params)
+    if @company.save
+      flash[:notice] = 'Empresa cadastrada com sucesso.'
+      redirect_to root_path
+    else
+      flash[:notice] = 'Empresa cadastrada com sucesso.'
+      render :new
+    end
   end
 
   def update
     
   end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :url, :logo)
+  end
+  
   
 end
