1995fbf89f0d62ee4da531f5b018ca3382cca828
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2022-05-29 18:18:46 -0300

    Developed screens for registration of vacancies

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,8 @@
 class Position < ApplicationRecord
   belongs_to :company
+
+  enum career: [:developer, :business_inteligence, :information_tecnology, :desing,
+                :product, :tecnology, :other]
+  enum contract:[:clt, :pj, :match]
+  validates :name, :career, :contract, :city, :state, :summary, presence: true
 end
