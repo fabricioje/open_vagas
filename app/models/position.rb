1fa9de8596c41f8ea7cc949922110303dfdbb838
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2022-10-03 16:45:14 -0300

    * Melhoria do I18n em positions_controller * Instalado e configurado o Action Text * Implementado contador e limitador do campo Resumo da descrição

--- app/models/position.rb
+++ app/models/position.rb
@@ -4,5 +4,6 @@ class Position < ApplicationRecord
   enum career: [:developer, :business_inteligence, :information_tecnology, :desing,
                 :product, :tecnology, :other]
   enum contract:[:clt, :pj, :match]
+  has_rich_text :description
   validates :name, :career, :contract, :city, :state, :summary, presence: true
 end
