c1953a94400ab0635bee0d05273f8de3a49673a5
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2022-10-15 13:13:23 -0300

    Implementado verificação para exibir o formuário de cadastro de vagas, somente se tiver logado e não tiver se candidatado antes

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -35,8 +35,8 @@ class PositionsController < ApplicationController
   end
 
   def public_position
-    @position = Position.find_by(slug: params[:slug])
-    @applicant = current_user.applicants.new(position_id: @position.id)
+    @position = Position.find_by(slug: params[:slug])    
+    @applicant = current_user.applicants.new(position_id: @position.id) if user_signed_in?
   end
   
   
